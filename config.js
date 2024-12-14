7e0498cd6005551d9e561ab1f315d0ff69aa3189
Immanuel999-felix <emmanuelfelix829@gmail.com>
2024-12-14 06:00:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/08rco1.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, Immanuel999-bot-MD is alive now!!🙂",
 };
