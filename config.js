1918427453847ed541f296047f0621fb7df4d9fa
Immanuel999-felix <emmanuelfelix829@gmail.com>
2024-12-15 17:03:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID ||
+"bR5kQCpT#N4VMlGgx4iiMQlCd0jsDSmDnxBfqrn8PLMHRUoTorZA" ,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/08rco1.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, Immanuel999-bot-MD is alive now!!🙂",
 };
